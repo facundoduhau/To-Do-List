cedba04b46a06d4246c289a0724410805d0b2b9d
facundoduhau <duhaufacundo@gmail.com>
2025-09-22 14:07:50 -0300

    Worked on the task array

--- src/script.js
+++ src/script.js
@@ -1,3 +1,24 @@
-const list = document.getElementById('app-list')
-const addButton = document.getElementById('add-item')
-const addInput = document.getElementById('input-item')
+const taskList = document.getElementById('app-list')
+const addTaskButton = document.getElementById('add-item')
+const userInput = document.getElementById('input-item')
+
+let currentList = [];
+
+addTaskButton.addEventListener("click", ()=>{
+    const userText = userInput.value.trim();
+    if (userText === ""){
+        return;
+    };
+
+    const newTask ={
+        id: Date.now(),
+        text: userText,
+        completed: false
+    }
+
+    currentList.push(newTask);
+
+    userInput.value = "";
+
+    console.log(currentList);
+})
