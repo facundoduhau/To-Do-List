a5de4f83ffc81c3acd8436156f4ff4bd56b28053
Facundo <duhaufacundo@gmail.com>
2025-10-01 20:03:23 -0300

    Finished working on local storage, and a function to render each task to eather stringify or parse it from/to JSON

--- src/script.js
+++ src/script.js
@@ -1,24 +1,60 @@
-const taskList = document.getElementById('app-list')
-const addTaskButton = document.getElementById('add-item')
-const userInput = document.getElementById('input-item')
+document.addEventListener("DOMContentLoaded", ()=>{
+    const taskList = document.getElementById('app-list')
+    const addTaskButton = document.getElementById('add-item')
+    const userInput = document.getElementById('input-item')
 
-let currentList = [];
+    let currentList = JSON.parse(localStorage.getItem('list')) || []; // What does this even mean?
+    // It means it will first try and read local storage strings, and parse it to JSON format
+    // If the local Storage is empty, it will then initialize the array with an empty list
 
-addTaskButton.addEventListener("click", ()=>{
-    const userText = userInput.value.trim();
-    if (userText === ""){
-        return;
-    };
+    currentList.forEach(task => { // We run a forEach loop to travel all the array and run the render task function on each item
+        renderTasks(task)
+    });
 
-    const newTask ={
-        id: Date.now(),
-        text: userText,
-        completed: false
+    addTaskButton.addEventListener("click", ()=>{
+        const userText = userInput.value.trim();
+        if (userText === ""){
+            return;
+        };
+
+        const newTask ={
+            id: Date.now(),
+            text: userText,
+            completed: false
+        }
+
+        currentList.push(newTask);
+
+        saveTasks();
+
+        userInput.value = "";
+
+        console.log(currentList);
+    })
+
+    // I want to save these tasks in the local storage
+    // How can I make it so that these objects are saved?
+    // We make a function that accesses the local storage API from the browser
+    // In this case, it's <localStorage>
+    // Yeah... how original
+
+    function saveTasks(){
+        localStorage.setItem('list', JSON.stringify(currentList)) // <setItem> PUSHES to local Storage. You need to send a string
     }
 
-    currentList.push(newTask);
+    // Now I can add this function to line (24) to save a task to the list of the local Storage API.
 
-    userInput.value = "";
+    // The thing is, when you reload, or hit (F5), you get the local storage again but...
+    // When you add new tasks it rewrites the local storage, meaning, it treats it as an empty array where you can see past items
 
-    console.log(currentList);
+    // To fix this, we need to read the Local Storage, and render it's contents to the DOM
+
+    // But we want to do this as soon as the web is loading, since we want to add the local Storage items to
+    // the array list we have
+
+    // The solution is to wrap everything inside a document event, listening for the DOM loading
+
+    function renderTasks(task){
+        console.log(task);
+    }
 })
